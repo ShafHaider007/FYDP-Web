13fa212ba6f62079c43370555dcf7d873ea583e5
shaf_haider007 <shaf.shafheidar@gmail.com>
2024-11-22 06:47:36 +0500

    setting up the base

--- urbanMobility/frontend/src/App.jsx
+++ urbanMobility/frontend/src/App.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import HomePage from "./pages/Homepage/HomePage";
+import Header from "./components/common/Header/Header";
+import Footer from "./components/common/Footer/Footer";
+import "./App.css";
 
 function App() {
   return (
-    <div>
-     
+    <div className="app-container">
+      <Header />
+      <HomePage />
+      <Footer />
     </div>
   );
 }
