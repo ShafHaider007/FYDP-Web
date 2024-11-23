7e858b5034f6f9eeba18854f889b99297927c182
tahreemfatima7 <tahreemfatima703@gmail.com>
2024-11-23 20:23:23 +0500

    header changes

--- urbanMobility/frontend/src/components/common/Header/Header.jsx
+++ urbanMobility/frontend/src/components/common/Header/Header.jsx
@@ -2,22 +2,29 @@ import React from "react";
 import "./Header.css";
 
 const Header = () => {
+  const scrollToSection = (sectionId) => {
+    document.getElementById(sectionId).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  };
+
   return (
     <header className="site-header">
       <div className="logo">Move Smart</div>
       <nav>
         <ul className="nav-links">
-          <li>
-            <a href="/">Home</a>
+          <li onClick={() => scrollToSection("home-section")}>
+            <a href="#home-section">Home</a>
           </li>
-          <li>
-            <a href="/about">About</a>
+          <li onClick={() => scrollToSection("about-section")}>
+            <a href="#about-section">About</a>
           </li>
-          <li>
-            <a href="/services">Services</a>
+          <li onClick={() => scrollToSection("services-section")}>
+            <a href="#services-section">Services</a>
           </li>
-          <li>
-            <a href="/contact">Contact</a>
+          <li onClick={() => scrollToSection("contact-section")}>
+            <a href="#contact-section">Contact</a>
           </li>
         </ul>
       </nav>
